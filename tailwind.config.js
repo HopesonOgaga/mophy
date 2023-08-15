e1c0f3edcd263deefbc16f30ffe22be0bba79822
Hopeson <hopesonogaga@gmail.com>
2023-08-15 17:03:20 +0100

    ui nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'search': "url('/build/icons/search.png')",
+      }
+    },
   },
   plugins: [],
 }
